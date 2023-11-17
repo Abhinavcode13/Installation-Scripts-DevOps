2f37b1545cd6f283fdac7062133bb1dfe1c1916d
Abhinav kumar <126642111+Abhinavcode13@users.noreply.github.com>
2023-11-17 18:35:20 +0530

    Create Sonarqube.sh

---
+++ Sonarqube.sh
@@ -0,0 +1,18 @@
+# Installation and configuration of SonarQube
+
+$ sudo apt-get update
+
+# Install java
+
+$ sudo apt install openjdk-17-jdk -y
+$ sudo apt install open-jdk-jre -y
+
+# Install wget and unzip package
+
+$ sudo apt-get install wget unzip -y
+
+# Download SonarQube
+
+$ wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.0.65466.zip
+$ unzip sonarqube-9.9.0.65466.zip
+$ cd sonarqube-9.9.0.65466.zip
