e75c6f0f5d3db4da3122ab3ae00c93d91971330e
Abhinav kumar <126642111+Abhinavcode13@users.noreply.github.com>
2023-11-17 18:51:16 +0530

    Update Sonarqube.sh

--- Sonarqube.sh
+++ Sonarqube.sh
@@ -1,4 +1,6 @@
-# Installation and configuration of SonarQube
+# Installation and configuration of SonarQube (Default Port - 9000)
+# Login - Admin
+# Password - Admin
 
 $ sudo apt-get update
 
@@ -16,3 +18,7 @@ $ sudo apt-get install wget unzip -y
 $ wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.0.65466.zip
 $ unzip sonarqube-9.9.0.65466.zip
 $ cd sonarqube-9.9.0.65466.zip
+$ cd bin
+$ cd linux-x86-64
+$ ./sonar.sh
+$ ./sonar.sh console
