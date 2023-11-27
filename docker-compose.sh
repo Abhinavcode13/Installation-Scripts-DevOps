f86ddcb7a9c9da4b1678df09b57957a02555ebc6
Abhinav kumar <126642111+Abhinavcode13@users.noreply.github.com>
2023-11-27 09:53:35 +0530

    Update and rename docker-compose.txt to docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -4,3 +4,22 @@
 -> Running all/multiple containers manually is a difficult task.
 
 -> To solve this problem we use docker-compose , docker compose is a tool which is used manage multi container based applications.
+
+-> docker compose filename : docker-compose.yml
+
+sample docker-compose.yml file
+
+version :
+services :
+network :
+volumes :
+
+#Create and start the containers
+$ docker-compose up
+# List docker containers started by docker containers
+$ docker-compose ps
+#stop or remove containers
+$ docker-compose down
+#list down running docker images
+$ docker-compose images
+
