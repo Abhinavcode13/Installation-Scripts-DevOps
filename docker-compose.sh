7d6f69657eb287052938096a3453d8d12eece5a2
Abhinav kumar <126642111+Abhinavcode13@users.noreply.github.com>
2023-11-27 09:41:33 +0530

    Create docker-compose.sh

---
+++ docker-compose.sh
@@ -0,0 +1,6 @@
+-> In real world applications are developed using microservices architecture.
+-> In microservices architecture several apis will be available.
+-> Every API is project and it should run using a contanier.
+-> Running all/multiple containers manually is a difficult task.
+
+-> To solve this problem we use docker-compose , docker compose is a tool which is used manage multi container based applications.
