c11267d06989536eeaaf364fa4dd71504c610ed7
Abhinav kumar <126642111+Abhinavcode13@users.noreply.github.com>
2023-12-05 21:21:32 +0530

    Update Docker-Swarm.sh

--- Docker-Swarm.sh
+++ Docker-Swarm.sh
@@ -18,3 +18,21 @@ To install master node in the master server command is
 $ sudo docker swarm init --avertise-addr <public-ip-address-of-ubuntu-machine>
 $ sudo docker swarm join-token worker - To add master node in the machine
 $ sudo docker swarm join-token worker - To add worker node in the machine
+
+Docker swarm service
+++++++++++++++++++++
+
+-> Service is a collection of one or more service of same image.
+There are two types of service in docker swarm
+1) Default
+2) Global
+
+Command to create the service 
+$ sudo docker service create --name <service-name> -p <host-port:containerport> <image-name>
+(By default only 1 replica is created)
+
+#To check the services
+$ docker service ls
+
+#To scale the docker service
+$ sudo docker service scale <service-name>=<no of replica>
